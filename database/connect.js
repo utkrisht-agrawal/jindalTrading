04153b980fdf6bf2ec40ccd0f2123f50bed7851e
kiosop <104259533+kiosop@users.noreply.github.com>
2024-03-19 08:46:14 +0530

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('jindaltrading','admin','clashlone##00Q',{
     dialect: 'mysql',
-    host: 'jindaltradingdatabase.c7a4v7gkjhri.ap-south-1.rds.amazonaws.com'
+    host: 'jindaltradingdatabase.c7a4v7gkjhri.ap-south-1.rds.amazonaws.com' 
 });
 
 module.exports = sequelize;
